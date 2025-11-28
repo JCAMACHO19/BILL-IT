e4acb1248145ccb250af6424d3d9052508a1838b
JCAMACHO19 <JCAMACHO19@users.noreply.github.com>
2025-11-28 15:05:37 +0100

    Update app.py - minor comment modification

--- app.py
+++ app.py
@@ -136,6 +136,6 @@ else:
     # Si no está logueado, mostrar solo la pestaña de Inicio
     st.write("Por favor, inicie sesión para acceder al resto de las funcionalidades.")
 
-#streamlit run app.py
+#streamlit run app.py ------
 
 
